6d754d278074acedefb1962de10ec9c5265f3bed
TaewooKim <embedded61@gmail.com>
2021-11-19 10:37:52 +0900

    [feature#3] Add: docker 개발용 설정 추가 - commands app 추가   - mysql 환경 설정 dev_local 파일에 추가 완료   - wait_for_db : db가 django와 연결될때까지 대기 하는 명령어 연결된것을     확인후, migrate 실행한다.

--- deer/settings/dev_local.py
+++ deer/settings/dev_local.py
@@ -6,4 +6,16 @@ DEBUG = True
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'SECRET_KEY')
 
-# Todo: DB setting.... 
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.contrib.gis.db.backends.mysql',
+        'NAME': os.environ.get('SQL_DATABASE', 'deer'),
+        'USER': os.environ.get('SQL_USER', 'user-deer'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD', 'password-deer'),
+        'HOST':  os.environ.get('SQL_HOST', 'localhost'),
+        'PORT': os.environ.get('SQL_PORT', 3306),
+        'OPTIONS' : {
+            'charset' : 'utf8mb4'
+        }
+    }
+}
