391ec9733ae1b2c1ae7d646876c27116622a3858
TaewooKim <embedded61@gmail.com>
2021-11-20 22:00:44 +0900

    [feature #7] Add: Parkingzoon 및 forbidden_area model 추가

--- area/models.py
+++ area/models.py
@@ -12,3 +12,20 @@ class Area(models.Model):
     fee_per_min = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Parkingzone(models.Model):
+    class Meta:
+        db_table = 'parkingzones'
+
+    center_lat = models.DecimalField(max_digits=17, decimal_places=14)
+    center_lng = models.DecimalField(max_digits=17, decimal_places=14)
+    radius     = models.FloatField()
+
+
+class ForbiddenArea(models.Model):
+    class Meta:
+        db_table = 'forbidden_areas'
+
+    boundary = models.PolygonField()
+    coords   = models.MultiPointField()
